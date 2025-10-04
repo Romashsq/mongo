c3f971c91baa12178a5896bbb5e7654d6e11bc45
Roman <romanbond1111@gmail.com>
2025-10-04 11:35:35 +0200

    com for dz

--- express-mongo-crud/index.js
+++ express-mongo-crud/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json());
 
-// Підключення до MongoDB
+app.use(express.json());
+app.use(cors());
+
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,16 +18,16 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log("✅ MongoDB Atlas connected"))
 .catch(err => console.error("❌ MongoDB connection error:", err));
 
-// Підключення роутів
+
 const usersRoute = require("./routers/users.js");
 app.use("/users", usersRoute);
 
-// Root route
+
 app.get("/", (req, res) => {
     res.send("🚀 Сервер працює! CRUD доступний на /users");
 });
 
-// Запуск сервера
+
 app.listen(PORT, () => {
     console.log(`✅ Сервер запущено: http://localhost:${PORT}`);
 });
